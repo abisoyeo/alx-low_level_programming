d4d1d95add47f3299246da18f88f12767d58123a
Abisoye <ogunmonaa@gmail.com>
2023-02-16 12:16:48 -0800

    commiting from git script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - main function of program
+ * Return: returns 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
