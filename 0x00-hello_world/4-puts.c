66f3ff149573cd70ca4659c905052e8f804b6527
Abisoye <ogunmonaa@gmail.com>
2023-02-16 11:56:23 -0800

    commiting from git script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main function
+ * /
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
